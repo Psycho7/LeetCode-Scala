39b7b69b0062b65d91fc6f82bc5320b5427c2b74
Chujie Zeng <chujie.zeng@gmail.com>
2017-10-27 22:30:35 -0400

    Two Sum

---
+++ Easy/two-sum.scala
@@ -0,0 +1,11 @@
+object Solution {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    def iter(pos: Int, map: Map[Int, Int]): Array[Int] = {
+      val self = nums(pos)
+      val other = target - self
+      if (map contains other) Array(map(other), pos)
+      else iter(pos + 1, map + (self -> pos))
+    }
+    iter(0, Map[Int, Int]())
+  }
+}
